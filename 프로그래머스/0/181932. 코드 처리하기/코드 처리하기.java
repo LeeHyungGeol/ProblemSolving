6d550b6fbaeea0bd4243c2c79341853b77c8938b
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-24 03:40:07 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -3,19 +3,13 @@ class Solution {
         String answer = "";
         int mode = 0;
         for(int i = 0; i < code.length(); ++i) {
-            String c = String.valueOf(code.charAt(i));
-            if (mode == 0) {
-                if (c.equals("1")) {
-                    mode = 1;
-                } else {
-                    answer += i % 2 == 0 ? c : "";
-                }
-            } else {
-                if (c.equals("1")) {
-                    mode = 0;
-                } else {
-                    answer += i % 2 == 0 ? "" : c;
-                }
+            char c = code.charAt(i);
+            if (c == '1') {
+                mode = mode == 1 ? 0 : 1;
+                continue;
+            }
+            if (i%2 == mode) {
+                answer += c;
             }
         }
         return answer.isEmpty() ? "EMPTY" : answer;
