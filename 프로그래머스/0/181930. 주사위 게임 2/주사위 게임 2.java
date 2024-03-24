9adbd0c19bd59512755b9c3b235be11a511fdae0
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-24 16:43:39 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;
-        boolean ab = a == b;
-        boolean ac = a == c;
-        boolean bc = b == c;
-        if (ab && bc && ac) {
-            answer += (a+b+c) * (Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2)) * (Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
-        } else if (ab || bc || ac) {
-            answer += (a+b+c) * (Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+        if (a==b && b==c) {
+            answer = (a+b+c) * (a*a+b*b+c*c) * (a*a*a+b*b*b+c*c*c);
+        } else if (a==b || b==c || a==c) {
+            answer = (a+b+c) * (a*a+b*b+c*c);
         } else {
-            answer += (a+b+c);
+            answer = (a+b+c);
         }
         return answer;
     }
