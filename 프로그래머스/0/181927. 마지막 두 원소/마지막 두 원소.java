6c0ea2614663905266190d7bac8da736bac41b8f
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-25 00:45:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 3.52 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -4,8 +4,9 @@ import java.lang.Integer;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> numList = Arrays.stream(num_list).boxed().collect(Collectors.toList());
-       numList.add(num_list[num_list.length-1] > num_list[num_list.length-2] ? num_list[num_list.length-1]-num_list[num_list.length-2] : num_list[num_list.length-1]*2);
-        return numList.stream().mapToInt(i->i).toArray();
+        return IntStream.iterate(0, i -> i + 1)
+                        .limit(num_list.length + 1)
+                        .map(i -> i == num_list.length ? (num_list[i - 1] > num_list[i - 2] ? num_list[i - 1] - num_list[i - 2] : 2 * num_list[i - 1]) : num_list[i])
+                        .toArray();
     }
 }
