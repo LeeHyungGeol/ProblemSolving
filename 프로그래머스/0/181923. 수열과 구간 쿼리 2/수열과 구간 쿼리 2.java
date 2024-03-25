b5492a2f150b3408cc6937225cb2d61c3c8d5cab
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-26 03:05:52 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 10.74 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -2,13 +2,12 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    public List<Integer> solution(int[] arr, int[][] queries) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for(int i = 0; i < queries.length; ++i) {
-            int s = queries[i][0], e = queries[i][1], k = queries[i][2];
-            answer.add(Arrays.stream(arr).skip(s).limit(e-s+1).sorted().filter(n -> n > k).findFirst().orElse(-1));
-        }
-        return answer;
+    public int[] solution(int[] arr, int[][] queries) {
+         return IntStream.range(0, queries.length)
+                .map(q -> IntStream.rangeClosed(queries[q][0], queries[q][1])
+                        .map(i -> arr[i])
+                        .filter(i -> i > queries[q][2])
+                        .min().orElse(-1)
+                ).toArray();
     }
 }
