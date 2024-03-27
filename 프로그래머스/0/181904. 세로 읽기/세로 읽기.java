2add9ba49e552e467bc068bf09b9dcd19397fffd
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-28 01:53:01 +0900

    [level 0] Title: 세로 읽기, Time: 4.15 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -3,6 +3,6 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string, int m, int c) {
-        return IntStream.iterate(c-1, i <= my_string.length(), i -> i + m).mapToObj(idx -> my_string.charAt(idx)).collect(Collectors.joining());
+        return IntStream.iterate(c-1, i -> i <= my_string.length()-1, i -> i + m).mapToObj(idx -> String.valueOf(my_string.charAt(idx))).collect(Collectors.joining());
     }
 }
