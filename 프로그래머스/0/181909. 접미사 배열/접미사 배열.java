62ff847b3dcaf69b5c2dd57e8eba43e7efbe9f47
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-28 01:11:18 +0900

    [level 0] Title: 접미사 배열, Time: 2.15 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,9 +1,7 @@
-import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.Collectors;
 
 class Solution {
-    public List<String> solution(String my_string) {
-        return IntStream.range(0, my_string.length()).mapToObj(i -> my_string.substring(i)).sorted().collect(Collectors.toList());
+    public String[] solution(String my_string) {
+        return IntStream.range(0, my_string.length()).mapToObj(i -> my_string.substring(i)).sorted().toArray(String[]::new);
     }
 }
