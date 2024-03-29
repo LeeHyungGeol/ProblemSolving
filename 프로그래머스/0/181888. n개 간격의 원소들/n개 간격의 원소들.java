75f900bf38decf27667ab8a82c65c042cac4c018
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-30 02:45:12 +0900

    [level 0] Title: n개 간격의 원소들, Time: 3.05 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.iterate(0, i -> i < num_list.length, i -> i+n).map(idx-> num_list[idx]).toArray();
+    }
+}
