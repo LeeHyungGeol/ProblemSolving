e136d7a971aae866ec1d399f11fdd639dc17c0fe
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-30 02:19:21 +0900

    [level 0] Title: 배열 만들기 3, Time: 3.86 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -3,6 +3,7 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        return IntStream.concat(Arrays.stream(arr, intervals[0][0], intervals[0][1]+1), Arrays.stream(arr, intervals[1][0], intervals[1][1]+1)).toArray();
+        return Arrays.stream(intervals).flatMapToInt(ints -> Arrays.stream(Arrays.copyOfRange(arr, ints[0], ints[1] + 1))).toArray();
+
     }
 }
