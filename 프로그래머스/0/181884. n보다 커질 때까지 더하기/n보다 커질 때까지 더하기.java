1a0efa9837b00b544e6616f0ae1d63af9f636ceb
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-03-31 16:49:30 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.73 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers, int n) {
+        return Arrays.stream(numbers).reduce(0, (acc, i) -> n >= acc ? acc + i : acc);
+    }
+}
