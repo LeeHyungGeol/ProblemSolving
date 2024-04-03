4a80e59cc294458c013f2fa404136a08ef120b42
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-03 23:39:38 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 2.34 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        return IntStream.of((int)Arrays.stream(num_list).filter(i->i%2==0).count(), (int)Arrays.stream(num_list).filter(i->i%2==1).count()).toArray();
+    }
+}
