3e64f7a01205c031d869e45d718eba2b96948fba
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-05 00:19:12 +0900

    [level 0] Title: x 사이의 개수, Time: 5.71 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String myString) {
-        return Arrays.stream(myString.split("x", myString.length())).mapToInt(str->str.length()).toArray();
+        // return Arrays.stream(myString.split("x", myString.length())).mapToInt(str->str.length()).toArray();
+        return Arrays.stream(myString.split("x", -1)).mapToInt(str->str.length()).toArray();
     }
 }
