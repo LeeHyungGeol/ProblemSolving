7abe82cb270986b2a18a71a5f17e8cbe7972996a
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-06 02:26:45 +0900

    [level 0] Title: 문자열 묶기, Time: 28.08 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public long solution(String[] strArr) {
+//         Map<Integer, Long> lengthCountMap = Arrays.stream(strArr)
+//                 .collect(Collectors.groupingBy(String::length, Collectors.counting()));
+
+//         return lengthCountMap.values().stream()
+//                 .max(Long::compare)
+//                 .orElse(0L);
+        
+        Map<Integer, Integer> lengthCountMap = new HashMap<>();
+        int answer = 0;
+
+        for (String str : strArr) {
+            lengthCountMap.put(str.length(), lengthCountMap.getOrDefault(str.length(), 0) + 1);
+        }
+
+        for (int count : lengthCountMap.values()) {
+            answer = Math.max(answer, count);
+        }
+
+        return answer;
+    }
+}
