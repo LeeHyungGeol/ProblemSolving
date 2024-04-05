63e1e88296eddac59d62c557db9150f45b38b587
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-05 19:09:01 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,5 +1,11 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        return numbers[(2 * (k - 1)) % numbers.length];
+        // return numbers[(2 * (k - 1)) % numbers.length];
+        int answer = 1, idx = 0;
+        for (int i = 0; i < k-1; ++i) {
+            idx += 2;
+            if (idx > numbers.length-1) idx -= numbers.length;
+        }
+        return numbers[idx];
     }
 }
