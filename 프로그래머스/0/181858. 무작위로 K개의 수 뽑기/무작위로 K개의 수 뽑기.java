69d5ac895a208a3f6c15020034b47d6f9fd87587
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-05 12:02:10 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 8.43 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        return IntStream.concat(Arrays.stream(arr).distinct(), IntStream.range(0, k).map(i -> -1)).limit(k).toArray();
+    }
+}
