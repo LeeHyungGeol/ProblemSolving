5d96b44e4af3cc49d40b1fd0f0be7b1c99b4936f
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-08 16:32:32 +0900

    [level 0] Title: 커피 심부름, Time: 2.26 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] order) {
+        return Arrays.stream(order).mapToInt(i -> i.contains("cafelatte") ? 5000 : 4500).sum();
+    }
+}
