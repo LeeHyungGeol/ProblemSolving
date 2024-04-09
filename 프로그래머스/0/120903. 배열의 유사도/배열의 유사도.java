063868704a6382f4ff8d4aa64e6af77c5d288b69
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-09 14:37:36 +0900

    [level 0] Title: 배열의 유사도, Time: 0.71 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+//         int answer = 0;
+        
+//         for (int i = 0; i < s1.length; ++i) {
+//             for (int j = 0; j < s2.length; ++j) {
+//                 if (s1[i].equals(s2[j]))
+//                     ++answer;
+//             }
+//         }
+//         return answer;
+        
+        // Arrays.stream(s1).collect(Collectors.toList());
+        List<String> list = Arrays.asList(s1);
+        return (int) Arrays.stream(s2).filter(list::contains).count();
+    }
+}
