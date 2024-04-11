86bac50dfce592d20a5a295187f5cd617a8dc4bb
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-11 21:45:41 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 3.52 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] numbers) {
-        return 45-Arrays.stream(numbers).sum();
+        // return 45-Arrays.stream(numbers).sum();
+        
+        return IntStream.range(0, 10).filter(i -> Arrays.stream(numbers).noneMatch(num -> i == num)).sum();
+
     }
 }
