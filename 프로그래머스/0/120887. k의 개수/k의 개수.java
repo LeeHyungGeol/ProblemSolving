0910ebe0f250812d64b7c126d842ff8704947767
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-13 10:33:39 +0900

    [level 0] Title: k의 개수, Time: 17.78 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,9 +1,14 @@
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int i, int j, int k) {
-        String str = "";
-        for (int l = i; l <= j; ++l) {
-	        str += String.valueOf(l);
-        }
-        return str.length() - str.replace(k+"", "").length();
+        // String str = "";
+        // for (int l = i; l <= j; ++l) {
+        // str += String.valueOf(l);
+        // }
+        // return str.length() - str.replace(k+"", "").length();
+        
+        return (int)IntStream.rangeClosed(i,j).filter(n->String.valueOf(n).contains(String.valueOf(k))).mapToObj(String::valueOf).collect(Collectors.joining()).chars().mapToObj(c->(char)c).filter(c -> c == (k + '0')).count();
     }
 }
