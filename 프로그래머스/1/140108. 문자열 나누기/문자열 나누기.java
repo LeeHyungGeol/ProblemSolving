62e36fbdad4b13fee45efb2dc16499a3a30ecc73
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-14 01:55:23 +0900

    [level 1] Title: 문자열 나누기, Time: 0.49 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,30 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        List<String> answer = new ArrayList<>();
-        String splitStr = "";
         char x = ' ';
-        int countX = 0, countOthers = 0;
+        int countX = 0, countOthers = 0, answer = 0;
         
         for (char c : s.toCharArray()) {
-            if (x==c || splitStr.isEmpty()) {
-                x = c;
-                ++countX;
-                splitStr += c;
-                continue;
-            }
-            splitStr += c;
-            ++countOthers;
-            if (x != c && countX == countOthers) {
-                answer.add(splitStr);
-                splitStr = "";
+            if (x == ' ') x=c;
+            if (x==c) ++countX;
+            else ++countOthers;
+            if (countX == countOthers) {
+                ++answer;
+                x = ' ';
                 countX = 0; 
-                countOthers = 0; 
+                countOthers = 0;
             } 
         }
-        if (!splitStr.isEmpty()) answer.add(splitStr);
+        if (countX != 0) ++answer;
         
-        return answer.size();
+        return answer;
     }
 }
