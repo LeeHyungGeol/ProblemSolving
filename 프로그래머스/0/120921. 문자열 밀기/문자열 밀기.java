b4ed1510037913e93f4798e8fc006888bcfa0e5e
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-14 12:45:54 +0900

    [level 0] Title: 문자열 밀기, Time: 11.16 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(String A, String B) {
-        return (B+B).indexOf(A);
+        // return (B+B).indexOf(A);
+        String tmp = A;
+        for (int i = 0; i < A.length(); ++i) {
+            if (tmp.equals(B)) return i;
+            tmp = tmp.charAt(A.length()-1) + tmp.substring(0, A.length()-1);
+        }
+        
+        return -1;
     }
 }
