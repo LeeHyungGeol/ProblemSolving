fe783f4ce8f76fccb7dc01d3bcabaf65496e8b4d
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-15 15:49:51 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 9.30 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -11,9 +11,11 @@ class Solution {
         
 //         return Long.parseLong(sb.reverse().toString());
         
-        return Long.parseLong(String.valueOf(n).chars().mapToObj(ch -> (char) ch)
-                .sorted(Comparator.reverseOrder())
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString());
+        // return Long.parseLong(String.valueOf(n).chars().mapToObj(ch -> (char) ch)
+                // .sorted(Comparator.reverseOrder())
+                // .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                // .toString());
+                return Long.parseLong(String.valueOf(n).chars().boxed().map(c->c-'0').sorted(Comparator.reverseOrder()).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString());
+
     }
 }
