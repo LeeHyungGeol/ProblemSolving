ca0eed56d0d3f28af5c4fe919abd584d1e166355
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-15 14:01:05 +0900

    [level 1] Title: 옹알이 (2), Time: 1.16 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -5,16 +5,23 @@ class Solution {
         List<String> list = new ArrayList<>(Arrays.asList("aya", "ye", "woo", "ma"));      
         int answer = 0;
         
-        for (String b : babbling) {
-            String tmp = "", same = "";
-            for (char c : b.toCharArray()) {
-                tmp += c;
-                if (list.contains(tmp) && !same.equals(tmp)) {
-                    same = tmp;
-                    tmp = "";
-                }
-            }
-            if (tmp.isEmpty()) ++answer;
+        // for (String b : babbling) {
+        //     String tmp = "", same = "";
+        //     for (char c : b.toCharArray()) {
+        //         tmp += c;
+        //         if (list.contains(tmp) && !same.equals(tmp)) {
+        //             same = tmp;
+        //             tmp = "";
+        //         }
+        //     }
+        //     if (tmp.isEmpty()) ++answer;
+        // }
+        
+        for(String str:babbling){
+            if(str.contains("ayaaya") || str.contains("yeye") || str.contains("woowoo") || str.contains("mama"))
+                continue;
+            if(str.replace("aya", "1").replace("ye", "1").replace("woo", "1").replace("ma", "1").replace("1", "").length() == 0)
+                answer++;
         }
         return answer;
     }
