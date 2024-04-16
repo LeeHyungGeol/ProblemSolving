f1a4644a9cb56d4a2e1b56eab3b8bd63360b5245
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-16 13:59:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.14 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -3,6 +3,8 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int n) {
-        return IntStream.range(0,n).mapToObj(i->i%2==0?"수":"박").collect(Collectors.joining());
+        // return IntStream.range(0,n).mapToObj(i->i%2==0?"수":"박").collect(Collectors.joining());
+        
+        return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
     }
 }
