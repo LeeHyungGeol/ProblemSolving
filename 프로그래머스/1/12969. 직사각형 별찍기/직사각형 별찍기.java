53e1f6d0bafd85a3975ff94efda08caa3134534a
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-17 13:18:31 +0900

    [level 1] Title: 직사각형 별찍기, Time: 149.38 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        IntStream.range(0, b).forEach(i -> System.out.println("*".repeat(a)));
+    }
+}
