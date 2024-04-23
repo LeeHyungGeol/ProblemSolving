e8798f3b59966b7714afd0922ab65f42a77649c8
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-23 19:04:53 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.31 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        LinkedList<Integer> list = new LinkedList<>();
+        Queue<Integer> pq = new PriorityQueue<>();
         int[] answer = new int[score.length];
         
         for(int i = 0; i < score.length; ++i) {
-            list.add(score[i]);
-            Collections.sort(list);
-            
-            if(list.size() > k) list.removeFirst();
-            answer[i] = list.getFirst();
+            pq.offer(score[i]);
+            if(pq.size() > k) pq.poll();
+            answer[i] = pq.peek();
         }
+        
         return answer;
     }
 }
