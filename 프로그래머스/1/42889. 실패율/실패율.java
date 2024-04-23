802acfb02e7cfc68cc8d10d0dbda22503a3b6e1b
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-04-23 20:20:50 +0900

    [level 1] Title: 실패율, Time: 0.77 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    class Failure {
+        private int stageNumber;
+        private double fail;
+        
+        public Failure(int stageNumber, double fail) {
+            this.stageNumber = stageNumber;
+            this.fail = fail;
+        }
+        
+        public int getStageNumber() {
+            return stageNumber;
+        }
+        
+        public double getFail() {
+            return fail;
+        }
+    }
+    
+    public int[] solution(int N, int[] stages) {
+        List<Failure> list = new ArrayList<>();
+        int[] answer = new int[N];
+        
+        for (int i = 1; i <= N; ++i) {
+            int notClear = 0, reach = 0;
+            
+            for (int j = 0; j < stages.length; ++j) {
+                if (stages[j] == i) ++notClear;
+                if (stages[j] >= i) ++reach;
+            }
+            if (reach == 0) list.add(new Failure(i, 0));
+            else list.add(new Failure(i, (double)notClear/reach));
+        }
+        
+        Collections.sort(list, new Comparator<Failure>(){
+            @Override
+            public int compare(Failure o1, Failure o2) {
+                if (o1.getFail() < o2.getFail()) return 1;
+                else if (o1.getFail() > o2.getFail()) return -1;
+                else return o1.getStageNumber() - o2.getStageNumber();
+            }
+        });
+        
+        int i = 0;
+        for (Failure f : list) answer[i++] = f.getStageNumber();
+
+        return answer;
+    }
+}
