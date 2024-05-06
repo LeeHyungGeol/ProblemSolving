ec79c28c3acc69fa0b7b5f0796ed684e7b10171d
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-05-06 17:23:29 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 4.10 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        int[] score = {0,3,2,1,0,1,2,3};
+        Map<Character, Integer> map = new HashMap<>() {{
+            put('R',0);
+            put('T',0);
+            put('C',0);
+            put('F',0);
+            put('J',0);
+            put('M',0);
+            put('A',0);
+            put('N',0);
+        }};
+        String answer = "";
+        
+        for (int i = 0; i < survey.length; ++i) {
+            if (choices[i] < 5) {
+                map.put(survey[i].charAt(0), map.get(survey[i].charAt(0))+score[choices[i]]);
+            } else {
+                map.put(survey[i].charAt(1), map.get(survey[i].charAt(1))+score[choices[i]]);
+            }
+        }
+        if (map.get('R') >= map.get('T')) answer +="R";
+        else answer += "T";
+        if (map.get('C') >= map.get('F')) answer +="C";
+        else answer += "F";
+        if (map.get('J') >= map.get('M')) answer +="J";
+        else answer += "M";
+        if (map.get('A') >= map.get('N')) answer +="A";
+        else answer += "N";
+        
+        return answer;
+    }
+}
