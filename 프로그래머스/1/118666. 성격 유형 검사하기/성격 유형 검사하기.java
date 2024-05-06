30fee928cd4edc086e3f9b1710815201fb69afa9
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-05-06 17:44:42 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 9.98 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
         int[] score = {0,3,2,1,0,1,2,3};
+        char[][] type = {{'R','T'}, {'C','F'}, {'J','M'}, {'A','N'}};
         Map<Character, Integer> map = new HashMap<>() {{
             put('R',0);
             put('T',0);
@@ -22,14 +23,18 @@ class Solution {
                 map.put(survey[i].charAt(1), map.get(survey[i].charAt(1))+score[choices[i]]);
             }
         }
-        if (map.get('R') >= map.get('T')) answer +="R";
-        else answer += "T";
-        if (map.get('C') >= map.get('F')) answer +="C";
-        else answer += "F";
-        if (map.get('J') >= map.get('M')) answer +="J";
-        else answer += "M";
-        if (map.get('A') >= map.get('N')) answer +="A";
-        else answer += "N";
+        // if (map.get('R') >= map.get('T')) answer +="R";
+        // else answer += "T";
+        // if (map.get('C') >= map.get('F')) answer +="C";
+        // else answer += "F";
+        // if (map.get('J') >= map.get('M')) answer +="J";
+        // else answer += "M";
+        // if (map.get('A') >= map.get('N')) answer +="A";
+        // else answer += "N";
+        
+        for (char[] t : type) {
+            answer += map.get(t[0]) >= map.get(t[1]) ? t[0] : t[1];
+        }
         
         return answer;
     }
