f530cce9abc018ad45a3b27af3eb2971e410df2d
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-08-22 20:12:10 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(String[] cloth : clothes) {
+            if(map.containsKey(cloth[1])) {
+                map.put(cloth[1], map.get(cloth[1])+1);
+            }
+            else {
+                map.put(cloth[1], 1);
+            }
+        }
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= (entry.getValue() + 1);
+        }
+
+        return answer-1;
+    }
+}
