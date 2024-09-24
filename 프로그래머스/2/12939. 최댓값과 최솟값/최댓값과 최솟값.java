5d037691dd34c3b153041a0754547cc4a6d72e08
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-09-24 16:33:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 15.66 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {        
-        String[] arr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).sorted().mapToObj(i-> i+"").toArray(String[]::new);
+        String[] arr = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).sorted().mapToObj(Integer::toString).toArray(String[]::new);
         return arr[0] + " " + arr[arr.length-1];
     }
 }
