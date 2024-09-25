83076700378879443d68bfc2360774b8ea9fe837
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-09-25 15:15:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.53 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int []A, int []B) {
-        int[] arrA = Arrays.stream(A).sorted().toArray();
-        int[] arrB = Arrays.stream(B).sorted().toArray();
         int answer = 0;
         
-        for (int i = 0; i < arrA.length; ++i) {
-            answer += arrA[i] * arrB[arrA.length-1-i];
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i = 0; i < A.length; ++i) {
+            answer += A[i] * B[A.length-1-i];
         }
         return answer;
     }
