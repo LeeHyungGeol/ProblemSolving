055adca313160f4bfeb601ecfe07f45249113ef6
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-09-27 16:51:34 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#qna) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 0.36 ms
+메모리: 83.6 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 22일 23:46:08
+2024년 09월 27일 16:51:31
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,12 +1,14 @@
 class Solution {
-    long[] dp = new long[2001];
     public long solution(int n) {
-        dp[0] = 0;
+        long answer = 0;
+        long[] dp = new long[2001];
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i < dp.length; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
+        
         return dp[n];
-  }
+    }
 }
