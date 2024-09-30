b746d5b97021bf1e9b0524bf3fc4ed0721bed187
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-01 02:05:29 +0900

    [level 2] Title: H-Index, Time: 7.05 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int n = citations.length, cnt = 0;
+        int cnt = 0;
         
         Arrays.sort(citations);
         
-        for(int h = 1; h <= n; ++h) {
-            for(int i = 0; i < n; ++i) {
-                if(h <= citations[i]) {
+        for (int h = 1; h <= citations.length; ++h) {
+            for (int i = 0; i < citations.length; ++i) {
+                if (h <= citations[i]) {
                     ++cnt;
                 }
             }
             
-            if(h <= cnt && n-cnt <= h) {
+            if (h <= cnt && citations.length-cnt <= h) {
                 answer = h;
             }
             
