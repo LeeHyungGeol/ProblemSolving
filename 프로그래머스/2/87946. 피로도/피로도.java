c576fe2cc4f420623c6931da6806eeb6e0332d4f
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-02 21:38:42 +0900

    [level 2] Title: 피로도, Time: 2.98 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,58 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int solution(int k, int[][] dungeons) {
-        int maxCount = 0;
-        int length = dungeons.length;
+        int n = dungeons.length;
 
-        // Create a list of indices to generate permutations
-        List<Integer> indices = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            indices.add(i);
-        }
+        Arrays.sort(dungeons, (a, b) -> (a[0] - a[1]) - (b[0] - b[1]));
 
-        // Generate all permutations of dungeon indices
-        List<List<Integer>> permutations = getPermutations(indices);
-        
-        // Evaluate each permutation
-        for (List<Integer> perm : permutations) {
-            int tk = k;
-            int count = 0;
+        int[][] dp = new int[n + 1][k + 1];
 
-            for (int i : perm) {
-                if (tk < dungeons[i][0]) {
-                    break;
+        for (int i = 1; i <= n; i++) {
+            for (int r = 1; r <= k; r++) {
+                if (dungeons[i - 1][0] > r) {
+                    dp[i][r] = dp[i - 1][r];
+                } else {
+                    dp[i][r] = Math.max(dp[i - 1][r], 1 + dp[i - 1][r - dungeons[i - 1][1]]);
                 }
-                tk -= dungeons[i][1];
-                count++;
             }
-
-            maxCount = Math.max(maxCount, count);
         }
 
-        return maxCount;
-    }
-
-    // Helper function to generate all permutations
-    private List<List<Integer>> getPermutations(List<Integer> list) {
-        List<List<Integer>> result = new ArrayList<>();
-        permuteHelper(list, 0, result);
-        return result;
-    }
-
-    // Helper function to recursively generate permutations
-    private void permuteHelper(List<Integer> list, int start, List<List<Integer>> result) {
-        if (start == list.size() - 1) {
-            result.add(new ArrayList<>(list));
-            return;
-        }
-
-        for (int i = start; i < list.size(); i++) {
-            Collections.swap(list, i, start);
-            permuteHelper(list, start + 1, result);
-            Collections.swap(list, i, start);
-        }
+        return dp[n][k];
     }
 }
