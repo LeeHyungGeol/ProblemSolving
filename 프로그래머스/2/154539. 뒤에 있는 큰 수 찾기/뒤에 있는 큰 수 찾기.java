06ca7c0d5c48b275dbe2bfd20f46681bb8aa4673
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-03 21:26:08 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 200.49 ms, Memory: 207 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,50 @@
+// n 이 1_000_000 이기 때문에, O(n^2) 인 완전 탐색으로는 시간 초과가 발생한다.
+// Stack 을 이용해야 한다.
+import java.util.*;
+
+class Solution {
+    
+    static class Node {
+        private int index;
+        private int value;
+        
+        public Node (int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+        
+        public int getIndex() {
+            return this.index;
+        }
+        
+        public int getValue() {
+            return this.value;
+        }
+    }
+    
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Stack<Node> stack = new Stack<>();
+        
+        for (int i = 0; i < numbers.length; ++i) {
+            int number = numbers[i];
+            
+            if (stack.isEmpty()) {
+                stack.push(new Node(i, number));
+            } else {
+                while (!stack.isEmpty() && number > stack.peek().getValue()) {
+                    Node node = stack.pop();
+                    answer[node.getIndex()] = number;
+                }
+                stack.push(new Node(i, number));
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            answer[node.getIndex()] = -1;
+        }
+        
+        return answer;
+    }
+}
