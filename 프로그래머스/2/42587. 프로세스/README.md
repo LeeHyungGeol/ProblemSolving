74189388c69913d7ed4b29e3c4331cc36ff92348
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-06 22:53:32 +0900

    [level 2] Title: 프로세스, Time: 1.07 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 53.2 MB, 시간: 2.90 ms
+메모리: 72 MB, 시간: 1.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 26일 20:33:11
+2024년 10월 06일 22:53:29
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,56 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        boolean flag = false;
-        Queue<Node> q = new LinkedList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i = 0; i < priorities.length; ++i) {
-            q.add(new Node(priorities[i], i));
-            pq.add(priorities[i]);
-        }
-        
-        while(true) {
-            Node fileSequence = q.peek();
-            int priority = pq.peek();
-            
-            if(priority > fileSequence.getPriority()) {
-                q.add(fileSequence);
-                q.poll();
-            }
-            else {
-                if(fileSequence.getSeq() == location) {
-                    flag = true;
-                }
-                pq.poll();
-                q.poll();
-                ++answer;
-            }
-            
-            if(flag) {
-                break;
-            }
-        }
-        
-        return answer;
-    }
-}
+        int answer = 1;
+        PriorityQueue p = new PriorityQueue<>(Collections.reverseOrder());;
 
-class Node {
-    private int priority;
-    private int seq;
-    
-    public Node(int priority, int seq) {
-        this.priority = priority;
-        this.seq = seq;
-    }
-    
-    public int getPriority() {
-        return this.priority;
-    }
-    
-    public int getSeq() {
-        return this.seq;
+        for(int i=0; i<priorities.length; i++){
+            p.add(priorities[i]);
+        }
+
+        while(!p.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)p.peek()){
+                    if(i == location){
+                        return answer;
+                    }
+                    p.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
     }
 }
