f26267a58ebe5091af6257e31513036f812f7aba
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-09 20:23:24 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 70.7 MB, 시간: 0.61 ms
+메모리: 78.2 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 01일 01:23:52
+2024년 10월 09일 20:23:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        Map<String, Set<String>> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < clothes.length; ++i) {
-            map.computeIfAbsent(clothes[i][1], k -> new HashSet<>()).add(clothes[i][0]);
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
         
-        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
-            answer *= entry.getValue().size()+1;
+        for (String clothType : map.keySet()) {
+            answer *= map.get(clothType) + 1;
         }
         
-        return --answer;
+        return answer - 1;
     }
 }
