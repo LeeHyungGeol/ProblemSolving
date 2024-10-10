a1a3a5e91f555a1fbc9ef5a9b2f1fc68c7c901ec
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-10 20:07:55 +0900

    [level 2] Title: 전화번호 목록, Time: 358.10 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 98.6 MB, 시간: 319.24 ms
+메모리: 98.7 MB, 시간: 358.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 21:42:19
+2024년 10월 10일 20:07:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,44 +1,15 @@
 import java.util.*;
 
 class Solution {
-     public boolean solution(String[] phone_book) {
-//         boolean answer = true;
-//         Map<String, Integer> map = new HashMap<>();
-        
-//         for(String phone : phone_book) {
-//             map.put(phone, 1);
-//         }
-        
-//         for(int i = 0; i < phone_book.length; ++i) {
-//             String temp = "";
-//             for(int j = 0; j < phone_book[i].length(); ++j) {
-//                 temp += phone_book[i].charAt(j);
-//                 if(map.containsKey(temp) && !temp.equals(phone_book[i])) {
-//                     return false;
-//                 }
-//             }
-//         }
-        
-//         return true;
-         
+    public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        boolean result = true;
-        for (int i=0; i<phone_book.length-1; i++) {
+        
+        for (int i = 0; i < phone_book.length-1; ++i) {
             if (phone_book[i+1].startsWith(phone_book[i])) {
-                result = false;
-                break;
+                return false;
             }
         }
-        return result;
-     }
-    
-//     public static boolean solution2(String[] phoneBook) {
-//         Arrays.sort(phoneBook);
-//         for(int i=0; i<phoneBook.length-1;i++) {
-//             if(phoneBook[i+1].startsWith(phoneBook[i])) {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
+        
+        return true;
+    }
 }
