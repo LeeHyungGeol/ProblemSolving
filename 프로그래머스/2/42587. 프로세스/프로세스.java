7344ac0bb71fe3f8f7f259fc617e6fe2501136ea
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-10 19:34:57 +0900

    [level 2] Title: 프로세스, Time: 1.18 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,26 +1,44 @@
 import java.util.*;
 
 class Solution {
+    
+    static class Node implements Comparable<Node> {
+        private int weight;
+        
+        public Node(int weight) {
+            this.weight = weight;
+        }
+        
+        public int getWeight() {
+            return this.weight;
+        }
+        
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(other.weight, this.weight);
+        }
+    }
+    
     public int solution(int[] priorities, int location) {
         int answer = 1;
-        PriorityQueue p = new PriorityQueue<>(Collections.reverseOrder());;
-
-        for(int i=0; i<priorities.length; i++){
-            p.add(priorities[i]);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < priorities.length; ++i) {
+            pq.add(new Node(priorities[i]));
         }
-
-        while(!p.isEmpty()){
-            for(int i=0; i<priorities.length; i++){
-                if(priorities[i] == (int)p.peek()){
-                    if(i == location){
+        
+        while(!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; ++i) {
+                if (priorities[i] == pq.peek().getWeight()) {
+                    if (i == location) {
                         return answer;
                     }
-                    p.poll();
-                    answer++;
+                    pq.poll();
+                    ++answer;
                 }
-            }
+            } 
         }
-
+        
         return answer;
     }
 }
