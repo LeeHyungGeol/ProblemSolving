90d01730caf4ee6ce3dc16a2a4edbf7198b35756
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-10-22 22:02:52 +0900

    [level 2] Title: 택배상자, Time: 434.46 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
 
 ### 성능 요약
 
-메모리: 154 MB, 시간: 84.69 ms
+메모리: 167 MB, 시간: 434.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 21:24:37
+2024년 10월 22일 22:02:50
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,27 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int answer = 0;
-        int[] priority = new int[order.length];
-        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        Queue<Integer> truck = new LinkedList<>();
+        Stack<Integer> assist = new Stack<>();
         
-        for(int i = 0; i < order.length; ++i){
-            priority[order[i] - 1] = i;
-        }
-        
-        for (int i = 0; i < priority.length; ++i) {
-            if (answer == priority[i]) {
-                ++answer;
-            } else {
-                stack.push(priority[i]);
-            }
-            while (!stack.isEmpty() && stack.peek() == answer) {
-                ++answer;
-                stack.pop();
+        for (int i = 0; i < order.length; ++i) {
+            assist.add(i+1);
+            
+            while (!assist.isEmpty()) {
+                if (assist.peek() == order[count]) {
+                    assist.pop();
+                    truck.add(i);
+                    ++count;
+                } else {
+                    break;
+                }
             }
         }
         
-        
-        return answer;
+        return truck.size();
     }
 }
