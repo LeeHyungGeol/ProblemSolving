ceb36b82597441ea60e8321beaebcaca91a4553a
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-11-04 16:18:23 +0900

    [level 2] Title: 소수 찾기, Time: 20.32 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,48 +1,47 @@
 import java.util.*;
 
 class Solution {
-    private static Set<Integer> set = new HashSet<>();
+    private static Set<Integer> set = new HashSet<>(); 
     
     public int solution(String numbers) {
         int answer = 0;
         boolean[] visited = new boolean[numbers.length()];
         
-        dfs(numbers, visited, "", 0);
-        
-        for (Integer number : set) {
-            if (isPrime(number)) {
-                ++answer;
-            }
+        for (int i = 1; i <= numbers.length(); ++i) {
+            dfs(numbers, "", visited, i);
         }
         
-        return answer;
+        return set.size();
     }
     
-    private void dfs(String numbers, boolean[] visited, String number, int depth) {
-        if (depth > numbers.length()) {
+    private void dfs(String numbers, String number, boolean[] visited, int size) {
+        if (size == number.length()) {
+            int n = Integer.parseInt(number);
+            if (isPrime(n)) {
+                set.add(n);
+            }
             return;
         }
- 
-        for (int i = 0; i < numbers.length(); i++) {
-            if(visited[i]) continue;
+        
+        for (int i = 0; i < numbers.length(); ++i) {
+            if (visited[i]) continue;
             visited[i] = true;
-            set.add(Integer.parseInt(number + numbers.charAt(i)));
-            dfs(numbers, visited, number + numbers.charAt(i), depth + 1);
+            dfs(numbers, number + numbers.charAt(i), visited, size);
             visited[i] = false;
         }
     }
     
-    public static boolean isPrime(int n) {
+    private boolean isPrime(int n) {
         if (n < 2) {
             return false;
         }
         
-        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+        for (int i = 2; i*i <= n; ++i) {
             if (n % i == 0) {
                 return false;
             }
         }
- 
+        
         return true;
     }
 }
