98ae3d5971a5704dc709023e1f02e8036dad81ca
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-11-22 16:21:17 +0900

    [level 2] Title: 후보키, Time: 1281.07 ms, Memory: 391 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,37 +1,85 @@
 import java.util.*;
 
 class Solution {
+    private static int ColumnSize = 0;
+    private static int RowSize = 0;
+    private static ArrayList<HashSet<Integer>> Candidates = new ArrayList<HashSet<Integer>>();
+    
     public int solution(String[][] relation) {
-        List<Integer> answer = new ArrayList<>();
-        int n = relation.length;
-        int m = relation[0].length;
+        RowSize = relation.length;
+        ColumnSize = relation[0].length;
         
-        for(int i = 1; i < (1<<m); ++i) {
-            Set<String> set = new HashSet<>();
-            for(int j = 0; j < n; ++j) {
-                String str = "";
-                for(int k = 0; k < m; ++k) {
-                    if((i & (1<<k)) > 0) {
-                        str += relation[j][k];
-                    }
-                }
-                set.add(str);         
+        for (int i = 0; i < ColumnSize; ++i) {
+            boolean[] visited = new boolean[ColumnSize];
+            dfs(relation, visited, 0, i+1);
+        }
+        
+        return Candidates.size();
+    }
+    
+    private void dfs(String[][] relation, boolean[] visited, int count, int size) {
+        if (count == size) {
+            HashSet<Integer> columnIndexes = getColumnIndexes(visited);
+            
+            if (!isUnique(relation, columnIndexes)) {
+                return;
             }
             
-            if(set.size() == n && check(answer, i)) {
-                answer.add(i);
+            if (!isMinimal(columnIndexes)) {
+                return;
+            }
+            
+            Candidates.add(columnIndexes);
+            return;
+        }
+        
+        for (int i = 0; i < ColumnSize; ++i) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs(relation, visited, count+1, size);
+            visited[i] = false;
+        }
+    }
+    
+    private boolean isUnique(String[][] relation, Set<Integer> columnIndexes) {
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < RowSize; ++i) {
+            StringBuilder sb = new StringBuilder();
+            
+            for (int columnIndex : columnIndexes) {
+                sb.append(relation[i][columnIndex]);
+            }
+            
+            if (list.contains(sb.toString())) {
+                return false;
+            } else {
+                list.add(sb.toString());
             }
         }
         
-        return answer.size();
+        return true;
     }
     
-    public boolean check(List<Integer> answer, int now) {
-        for(int i : answer) {
-            if((i & now) == i) {
+    private boolean isMinimal(Set<Integer> columnIndexes) {
+        for (Set<Integer> candidate : Candidates) {
+            if (columnIndexes.containsAll(candidate)) {
                 return false;
             }
         }
+        
         return true;
+    }
+    
+    private HashSet<Integer> getColumnIndexes(boolean[] visited) {
+        HashSet<Integer> indexes = new HashSet<>();
+        
+        for (int i = 0; i < ColumnSize; ++i) {
+            if (visited[i]) {
+                indexes.add(i);
+            }
+        }
+        
+        return indexes;
     }
 }
