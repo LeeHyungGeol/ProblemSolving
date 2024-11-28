c8543bcae611ab8f90f827a8511ec073f9ff052b
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-11-28 21:09:00 +0900

    [level 1] Title: 신고 결과 받기, Time: 180.45 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, Integer> reportedCounts = new HashMap<>();
+        Map<String, Set<String>> reportedInfo = new HashMap<>();
+        Map<String, Set<String>> banned = new HashMap<>();
+        
+        for (String r : report) {
+            String[] split = r.split(" ");
+            String reporter = split[0];
+            String reported = split[1];
+            
+            boolean flag = true;
+            
+            if (reportedInfo.containsKey(reporter) && reportedInfo.get(reporter).contains(reported)) {
+                flag = false;
+            }
+            reportedInfo.computeIfAbsent(reporter, key -> new HashSet<>()).add(reported);                
+            
+            if (flag) {
+                reportedCounts.put(reported, reportedCounts.getOrDefault(reported, 0) + 1);
+            }
+        }
+        
+        int index = 0;
+        for (String reporter : id_list) {
+            int count = 0;
+            if (reportedInfo.containsKey(reporter)) {                
+                for (String reported : reportedInfo.get(reporter)) {
+                    if (reportedCounts.get(reported) >= k) {
+                        ++count;
+                    }
+                }
+            }
+            answer[index++] = count;
+        }
+        
+        return answer;
+    }
+}
