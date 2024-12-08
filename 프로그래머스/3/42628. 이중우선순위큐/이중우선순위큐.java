abe5584961ac2c8f88be00b7a53641359824237d
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2024-12-08 21:33:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 74.98 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,25 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        Queue<Integer> minpq = new PriorityQueue<>();
-        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> min = new PriorityQueue<>();
+        Queue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
 
         for (String operation : operations) {
-            if (operation.startsWith("I ")) {
-                int n = Integer.parseInt(operation.substring(2));
-                minpq.offer(n);
-                maxpq.offer(n);
-            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
-                maxpq.remove(minpq.poll());
-            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
-                minpq.remove(maxpq.poll());
+            String[] split = operation.split(" ");
+            String command = split[0];
+            int value = Integer.parseInt(split[1]);
+            
+            if (command.equals("I")) {
+                min.offer(value);
+                max.offer(value);
+            } else {
+                if (!max.isEmpty() && value == -1) {
+                    max.remove(min.poll());
+                }
+                else if (!min.isEmpty() && value == 1) {
+                    min.remove(max.poll());
+                }
             }
         }
 
-        if (minpq.isEmpty() && maxpq.isEmpty()) {
+        if (max.isEmpty() && min.isEmpty()) {
             return new int[]{0, 0};
         }
 
-        return new int[]{maxpq.poll(), minpq.poll()};
+        return new int[]{max.poll(), min.poll()};
     }
 }
