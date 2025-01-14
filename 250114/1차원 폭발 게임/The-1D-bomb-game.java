1bc19f6dfb9cb9a4facb221508453e84761bfda3
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-01-14 20:23:58 +0900

    Title: 1차원 폭발 게임, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 250114/1차원 폭발 게임/The-1D-bomb-game.java
+++ 250114/1차원 폭발 게임/The-1D-bomb-game.java
@@ -1,70 +1,91 @@
 import java.util.*;
 
 public class Main {
+    private static final int BLANK = 0;
+    private static int N = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 입력 받기: n (리스트 크기), m (폭발 기준 개수)
         int n = sc.nextInt();
         int m = sc.nextInt();
-        List<Integer> arr = new ArrayList<>();
 
-        // 리스트 입력 받기
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr.add(sc.nextInt());
+            arr[i] = sc.nextInt();
         }
 
-        sc.close(); // Scanner 닫기
+        sc.close(); 
 
-        // 연쇄 폭발 시뮬레이션
+        N = n;
+            boolean isExploded = false;
+
         while (true) {
-            boolean didExplode = false;
+            isExploded = false;
 
-            // 리스트를 순회하며 폭탄 체크
-            int start = 0;
-            while (start < arr.size()) {
-                int num = arr.get(start);
-                if (num == 0) {
-                    start++;
+            for (int start = 0; start < N; ++start) {
+                if (arr[start] == 0) {
                     continue;
                 }
+                int end = getEndIndexOfExplosion(arr, start, arr[start]);
 
-                // 폭발 가능한 마지막 위치 찾기
-                int end = countExplodeBomb(arr, start, num);
-
-                // 연속된 폭탄 개수가 m 이상이면 폭발
-                if ((end - start + 1) >= m) {
-                    for (int i = start; i <= end; i++) {
-                        arr.set(i, 0); // 0으로 표시
-                    }
-                    didExplode = true;
+                if (end - start + 1 >= m) {
+                    bomb(arr, start, end);
+                    isExploded = true;
                 }
-
-                start = end + 1;
             }
 
-            // 0을 제거한 새로운 리스트 생성
-            arr.removeIf(x -> x == 0);
-
-            // 더 이상 폭발이 없으면 루프 종료
-            if (!didExplode) {
+            if (isExploded) {
+                gravity(arr);
+            } else {
                 break;
             }
         }
+        
+        System.out.println(N);
+        for (int i = 0; i < N; ++i) {
+            if (arr[i] != BLANK) {
+                System.out.println(arr[i]);
+            }
+        }
+        return;
+    }
 
-        // 결과 출력
-        System.out.println(arr.size());
-        for (int num : arr) {
-            System.out.println(num);
+    private static int getEndIndexOfExplosion(int[] arr, int start, int num) {
+        int end = start; 
+        for (int i = start + 1; i < N; ++i) {
+            if (arr[i] == num) {
+                end = i;
+            } else {
+                break;
+            }
+        }   
+        return end;
+    }
+
+    private static void bomb(int[] arr, int start, int end) {
+        for (int i = start; i <= end; ++i) {
+            arr[i] = BLANK;
         }
     }
 
-    // 특정 숫자(num)와 같은 값이 연속된 마지막 위치를 찾는 함수
-    private static int countExplodeBomb(List<Integer> arr, int start, int num) {
-        int end = start;
-        while (end + 1 < arr.size() && arr.get(end + 1) == num) {
-            end++;
+    private static void gravity(int[] arr) {
+        int count = 0;
+        int[] tempArr = new int[N];
+        int endOfTempArr = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (arr[i] != BLANK) {
+                tempArr[endOfTempArr++] = arr[i];
+            }
         }
-        return end;
+
+        N = endOfTempArr;
+
+        for (int i = 0; i < N; ++i) {
+            arr[i] = tempArr[i];
+        }
+
+
     }
 }
