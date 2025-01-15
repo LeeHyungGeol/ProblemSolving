2199f33da3759180ead0e370cec4eb088de5a000
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-01-15 17:03:45 +0900

    Title: 단 한 번의 2048 시도, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 250115/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 250115/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     private static final int BLANK = 0;
-    private static int N = 4;
+    private static int N = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,107 +20,81 @@ public class Main {
 
         sc.close();
 
-        gravityByDirection(arr, direction);
-        assembleNumber(arr, direction);
-        gravityByDirection(arr, direction);
+        N = n;
+
+        tilt(arr, direction);
         
         print(arr);
         
         return;
     }
 
-    private static void gravityByDirection(int[][] arr, char direction) {
+    private static void tilt(int[][] arr, char direction) {
+        int moveCount = 0;
+
         if (direction == 'L') {
-            for (int i = 0; i < 4; ++i) {
-                int[] temp = new int[4];
-                int endOfTempArr = 0;
-                for (int j = 0; j < 4; ++j) {
-                    if (arr[i][j] != BLANK) {
-                        temp[endOfTempArr++] = arr[i][j];
-                    }
-                }
-                for (int j = 0; j < 4; ++j) {
-                    arr[i][j] = temp[j];
-                }
-            }
+            moveCount = 3;
         } else if (direction == 'R') {
-            for (int i = 0; i < 4; ++i) {
-                int[] temp = new int[4];
-                int endOfTempArr = 3;
-                for (int j = 3; j >= 0; --j) {
-                    if (arr[i][j] != BLANK) {
-                        temp[endOfTempArr--] = arr[i][j];
-                    }
-                }
-                for (int j = 3; j >= 0; --j) {
-                    arr[i][j] = temp[j];
-                }
-            }
+            moveCount = 1;
         } else if (direction == 'U') {
-            for (int j = 0; j < 4; ++j) {
-                int[] temp = new int[4];
-                int endOfTempArr = 0;
-                for (int i = 0; i < 4; ++i) {
-                    if (arr[i][j] != BLANK) {
-                        temp[endOfTempArr++] = arr[i][j];
-                    }
-                }
-                for (int i = 0; i < 4; ++i) {
-                    arr[i][j] = temp[i];
+            moveCount = 2;
+        } else if (direction == 'D') {
+            moveCount = 0;
+        }
+
+        for (int i = 0; i < moveCount; ++i) {
+            rotate(arr);
+        }
+
+        gravity(arr);
+        assembleNumber(arr);
+        gravity(arr);
+
+        for (int i = 0; i < 4-moveCount; ++i) {
+            rotate(arr);
+        }
+    }
+
+    // grid를 시계방향으로 90' 회전시킵니다.
+    private static void rotate(int[][] arr) {
+        int[][] temp = new int[N][N];
+        // nextGrid를 0으로 초기화합니다.
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                temp[i][j] = 0;
+        
+        // 90' 회전합니다.
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                temp[i][j] = arr[N -j- 1][i];
+        
+        // nextGrid를 grid에 옮겨줍니다.
+         for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                arr[i][j] = temp[i][j];
+    }
+
+    private static void gravity(int[][] arr) {
+        for (int j = 0; j < N; ++j) {
+            int[] temp = new int[N];
+            int endOfTempArr = N-1;
+            for (int i = N-1; i >= 0; --i) {
+                if (arr[i][j] != BLANK) {
+                    temp[endOfTempArr--] = arr[i][j];
                 }
             }
-        } else if (direction == 'D') {
-            for (int j = 0; j < 4; ++j) {
-                int[] temp = new int[4];
-                int endOfTempArr = 3;
-                for (int i = 3; i >= 0; --i) {
-                    if (arr[i][j] != BLANK) {
-                        temp[endOfTempArr--] = arr[i][j];
-                    }
-                }
-                for (int i = 3; i >= 0; --i) {
-                    arr[i][j] = temp[i];
-                }
+            for (int i = N-1; i >= 0; --i) {
+                arr[i][j] = temp[i];
             }
         }
     }
 
-    private static void assembleNumber(int[][] arr, char direction) {
-        if (direction == 'L') {
-            for (int i = 0; i < 4; ++i) {
-                for (int j = 1; j < 4; ++j) {
-                    if (arr[i][j-1] == arr[i][j]) {
-                        arr[i][j-1] += arr[i][j];
-                        arr[i][j] = BLANK;
-                    }
-                }
-                
-            }
-        } else if (direction == 'R') {
-            for (int i = 0; i < 4; ++i) {
-                for (int j = 3; j > 0; --j) {
-                    if (arr[i][j-1] == arr[i][j]) {
-                        arr[i][j] += arr[i][j-1];
-                        arr[i][j-1] = BLANK;
-                    }
-                }
-            }
-        } else if (direction == 'U') {
-            for (int j = 0; j < 4; ++j) {
-                for (int i = 1; i < 4; ++i) {
-                    if (arr[i-1][j] == arr[i][j]) {
-                        arr[i-1][j] += arr[i][j];
-                        arr[i][j] = BLANK;
-                    }
-                }
-            }
-        } else if (direction == 'D') {
-            for (int j = 0; j < 4; ++j) {
-                for (int i = 3; i > 0; --i) {
-                    if (arr[i-1][j] == arr[i][j]) {
-                        arr[i][j] += arr[i-1][j];
-                        arr[i-1][j] = BLANK;
-                    }
+    private static void assembleNumber(int[][] arr) {
+        for (int j = 0; j < N; ++j) {
+            for (int i = N-1; i > 0; --i) {
+                if (arr[i-1][j] == arr[i][j]) {
+                    arr[i][j] += arr[i-1][j];
+                    arr[i-1][j] = BLANK;
                 }
             }
         }
