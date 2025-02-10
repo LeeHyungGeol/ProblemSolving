3b2b39205526e14fe8ff9e6a6d070ff4257e2107
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-11 00:33:16 +0900

    [level 0] Title: 안전지대, Time: 0.06 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/README.md
+++ 프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=java) 
 
 ### 성능 요약
 
-메모리: 69.9 MB, 시간: 2.02 ms
+메모리: 83.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 12일 00:56:22
+2025년 02월 11일 00:33:13
 
 ### 문제 설명
 
--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,50 +1,41 @@
 import java.util.*;
 
 class Solution {
-    class Point {
-        private int x;
-        private int y;
-        
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        
-        public int getX() {
-            return x;
-        }
-        
-        public int getY() {
-            return y;
-        }
-    }
+    static int N = 0;
+    static int[] DX = {-1,-1,-1,0,0,0,1,1,1};
+    static int[] DY = {-1,0,1,-1,0,1,-1,0,1};
     
     public int solution(int[][] board) {
-        int N = board.length;
-        int[] dx = {0,1,1,1,0,-1,-1,-1};
-        int[] dy = {1,1,0,-1,-1,-1,0,1};
-        List<Point> list = new ArrayList<>();
-        
-        // List<int[]> al = new ArrayList<>();
-        // al.add(new int[]{i, j});
+        int answer = 0;
+        N = board.length;
+        int[][] counter = new int[N][N];
         
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
-                if (board[i][j] == 1) 
-                    list.add(new Point(i,j));
+                if (board[i][j] == 1) {
+                    for (int d = 0; d < 9; ++d) {
+                        int nx = i + DX[d];
+                        int ny = j + DY[d];
+                        if (isInRange(nx,ny)) {
+                            counter[nx][ny] = 1;
+                        }
+                    }
+                }
             }
         }
         
-        for (Point p : list) {
-            for (int k = 0; k < 8; ++k) {
-                int nx = p.getX() + dx[k];
-                int ny = p.getY() + dy[k];
-                        
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] == 1) continue;
-                board[nx][ny] = 1;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (counter[i][j] == 1) {
+                    ++answer;
+                }
             }
         }
         
-        return (int) Arrays.stream(board).flatMapToInt(Arrays::stream).filter(i->i!=1).count();
+        return (N*N)-answer;
+    }
+    
+    private boolean isInRange(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
     }
 }
