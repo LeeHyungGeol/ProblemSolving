713058517c6c69e463368084b75d9ffc424d2328
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-12 17:32:45 +0900

    Title: 알파벳과 사칙연산, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250212/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+    static int Answer = 0;
+    static char[] Arr;
+    static int[] Nums;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String expression = sc.nextLine();
+        Arr = new char[expression.length()];
+
+        int i = 0, count = 0;
+        for (char c : expression.toCharArray()) {
+            if (Character.isLetter(c)) {
+                ++count;
+            }
+            Arr[i++] = c;
+        }
+
+        Nums = new int[count];
+
+        dfs(0);
+
+        System.out.println(Answer);
+    }
+
+    private static void dfs(int size) {
+        if (size == Nums.length) {
+            Answer = Math.max(Answer, calculate());
+            return;
+        }
+
+        for (int i = 1; i <= 4; ++i) {
+            Nums[size] = i;
+            dfs(size+1);
+            Nums[size] = 0;
+        }
+    }
+
+    private static int calculate() {
+        int result = Nums[0], numIndex = 1;
+
+        for (int i = 1; i < Arr.length; i += 2) {
+            if (Arr[i] == '+') {
+                result += Nums[numIndex++];
+            } else if (Arr[i] == '-') {
+                result -= Nums[numIndex++];
+            } else if (Arr[i] == '*') {
+                result *= Nums[numIndex++];
+            }
+        }
+
+        return result;
+    }
+}
