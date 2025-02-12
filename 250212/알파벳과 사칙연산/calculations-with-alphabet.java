d8b890cb3150530a76002b5b0e883949c3aa5303
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-12 17:49:31 +0900

    Title: 알파벳과 사칙연산, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 250212/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 250212/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -9,17 +9,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String expression = sc.nextLine();
-        Arr = new char[expression.length()];
-
-        int i = 0, count = 0;
-        for (char c : expression.toCharArray()) {
-            if (Character.isLetter(c)) {
-                ++count;
-            }
-            Arr[i++] = c;
-        }
-
-        Nums = new int[count];
+        
+        Arr = expression.toCharArray();
+        Nums = new int[6];
 
         dfs(0);
 
@@ -27,7 +19,7 @@ public class Main {
     }
 
     private static void dfs(int size) {
-        if (size == Nums.length) {
+        if (size == 6) {            
             Answer = Math.max(Answer, calculate());
             return;
         }
@@ -40,15 +32,15 @@ public class Main {
     }
 
     private static int calculate() {
-        int result = Nums[0], numIndex = 1;
+        int result = Nums[Arr[0]-'a'];
 
         for (int i = 1; i < Arr.length; i += 2) {
             if (Arr[i] == '+') {
-                result += Nums[numIndex++];
+                result += Nums[Arr[i+1]-'a'];
             } else if (Arr[i] == '-') {
-                result -= Nums[numIndex++];
+                result -= Nums[Arr[i+1]-'a'];
             } else if (Arr[i] == '*') {
-                result *= Nums[numIndex++];
+                result *= Nums[Arr[i+1]-'a'];
             }
         }
 
