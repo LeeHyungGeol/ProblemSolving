ca3f2ff887a91733a73212e18394ff708784963a
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-18 16:03:00 +0900

    Title: 수들의 합 최대화하기, Time: 3062ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

---
+++ 250218/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    static int N = 0, Answer = 0;
+    static int[][] Arr;
+    static boolean[] VisitedRow, VisitedCol;
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+
+        Arr = new int[N][N];
+        VisitedRow = new boolean[N];
+        VisitedCol = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                Arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        dfs(0, 0);
+
+        System.out.println(Answer);
+    }
+
+    private static void dfs(int depth, int sum) {
+        if (depth == N) {
+            Answer = Math.max(Answer, sum);
+            return;
+        }
+
+        for (int i = 0; i < N; ++i) {
+            if (VisitedRow[i]) continue;
+            for (int j = 0; j < N; ++j) {
+                if (VisitedCol[j]) continue;
+
+                VisitedRow[i] = true;
+                VisitedCol[j] = true;
+                dfs(depth+1, sum+Arr[i][j]);
+                VisitedRow[i] = false;
+                VisitedCol[j] = false;
+            }
+        }
+    }
+}
