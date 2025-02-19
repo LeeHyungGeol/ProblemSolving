77e2408b35b1f113aefd2558743742cd0f785e62
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-19 15:23:33 +0900

    Title: 계단 오르기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250219/계단 오르기/climbing-stairs.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    static final int MOD = 10_007;
+    static int N = 0;
+    static int[] dp = new int[1_001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        dp = new int[N+1];
+
+        dp[1] = 1 % MOD;
+        dp[2] = 1 % MOD;
+
+        for (int i = 3; i <= N; ++i) {
+            dp[i] = dp[i-2] + dp[i-3] % MOD;
+        }
+
+        System.out.println(dp[N]);  
+    }
+}
