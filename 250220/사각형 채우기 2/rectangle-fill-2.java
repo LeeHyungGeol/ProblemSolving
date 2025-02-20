56a963c43c624ab249fb16526fb7008d136f0fa8
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-20 16:38:36 +0900

    Title: 사각형 채우기 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250220/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    static final int MOD = 10_007;
+    static int N = 0;
+    static int[] dp = new int[1_001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= N; ++i) {
+            dp[i] = dp[i-1] + 2*dp[i-2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
