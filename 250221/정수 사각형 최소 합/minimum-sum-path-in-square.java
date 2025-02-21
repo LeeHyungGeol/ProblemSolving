477a2fca6499e0dc0375b58a0d60a110cda3c721
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-02-21 16:10:15 +0900

    Title: 정수 사각형 최소 합, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250221/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    static final int UNUSED = -1;
+    static int N = 0;
+    static int[][] Matrix, dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        Matrix = new int[N][N];
+        dp = new int[N][N];
+
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                Matrix[i][j] = sc.nextInt();
+                dp[i][j] = UNUSED;
+            }
+        }
+
+        System.out.println(findMinSum(0, N-1));
+    }
+
+    private static int findMinSum(int x, int y) {
+        if (dp[x][y] != UNUSED) {
+            return dp[x][y];
+        }
+        if (x == N-1 && y == 0) {
+            return dp[N-1][0] = Matrix[N-1][0];
+        }
+
+        int[] dx = {1,0};        
+        int[] dy = {0,-1};
+
+        int sum = 1_000_001;
+        for (int d = 0; d < 2; ++d) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (!isInRange(nx, ny)) continue;
+            sum = Math.min(sum, Matrix[x][y] + findMinSum(nx, ny));
+        }
+
+        dp[x][y] = sum;
+
+        return sum;
+    }
+
+    private static boolean isInRange(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
+    }
+}
