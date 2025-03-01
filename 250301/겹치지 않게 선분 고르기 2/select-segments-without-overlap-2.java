39c656a594c748a7a530578cc23c99e6342d638a
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-03-01 17:44:36 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 220ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 250301/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    static int N = 0, Answer = 1;
+    static Line[] Arr;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        Arr = new Line[N];
+        dp = new int[N];
+
+        for (int i = 0; i < N; ++i) {
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            Arr[i] = new Line(l,r);
+        }
+
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < N; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (Arr[j].r < Arr[i].l) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            Answer = Math.max(Answer, dp[i]);
+        }
+
+        System.out.println(Answer);
+    }
+
+    static class Line {
+        int l, r;
+
+        public Line(int l, int r) {
+            this.l = l;
+            this.r = r;
+        }
+    }
+}
