237fb96002d21b4b70b7af79d62e45b1b00bd4a6
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-04-30 19:00:47 +0900

    [level 2] Title: [3차] 압축, Time: 4.35 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,68 +1,36 @@
 import java.util.*;
 
 class Solution {
-    public static Map<String, Integer> dictionary = new HashMap<>(){{
-        put("A", 1);
-        put("B", 2);
-        put("C", 3);
-        put("D", 4);
-        put("E", 5);
-        put("F", 6);
-        put("G", 7);
-        put("H", 8);
-        put("I", 9);
-        put("J", 10);
-        put("K", 11);
-        put("L", 12);
-        put("M", 13);
-        put("N", 14);
-        put("O", 15);
-        put("P", 16);
-        put("Q", 17);
-        put("R", 18);
-        put("S", 19);
-        put("T", 20);
-        put("U", 21);
-        put("V", 22);
-        put("W", 23);
-        put("X", 24);
-        put("Y", 25);
-        put("Z", 26);
-    }};
+    private static Map<String, Integer> dict = new HashMap<>();
     
-    public List<Integer> solution(String msg) {
-        List<Integer> answer = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        msg = msg.toUpperCase();
-        int index = 27, next = 0;
+    public int[] solution(String msg) {
+        List<Integer> result = new ArrayList<>();
         
-        while(next < msg.length()) {
-            sb = new StringBuilder();
-            
-            for (int i = next; i < msg.length(); ++i) {
-                sb.append(msg.charAt(next));
-                String wc = sb.toString();
-            
-                if (!dictionary.containsKey(wc)) {
-                    answer.add(findIndex(getW(wc)));
-                    dictionary.put(wc, index++);
+        for (char c = 'A'; c <= 'Z'; ++c) {
+            dict.put(String.valueOf(c), c-'A'+1);
+        }
+        
+        int index = 27, left = 0;
+        while (left < msg.length()) {        
+            StringBuilder w = new StringBuilder();
+            for (int right = left; right < msg.length(); ++right) {
+                w.append(msg.charAt(right));
+                
+                if (!dict.containsKey(w.toString())) {
+                    dict.put(w.toString(), index++);
+                    if (w.length() > 1) {
+                        w.deleteCharAt(w.length()-1);
+                    }
+                    result.add(dict.get(w.toString()));
                     break;
                 }
-                ++next;
+                ++left;
             }
-            if (next == msg.length()) {
-                answer.add(findIndex(sb.toString()));
+            if (left == msg.length()) {
+                result.add(dict.get(w.toString()));
             }
         }
         
-        return answer;
-    }
-    
-    private String getW(String word) {
-        return word.substring(0, word.length()-1);
-    }
-    
-    private Integer findIndex(String w) {
-        return dictionary.get(w);
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
