a24c69cf3c2e6288a5da1f1d54e8bf0a6a8e1b78
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-05-09 14:53:55 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 9.28 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686#qna) 
 
 ### 성능 요약
 
-메모리: 81.3 MB, 시간: 10.61 ms
+메모리: 87.5 MB, 시간: 9.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 16:49:21
+2025년 05월 09일 14:53:52
 
 ### 문제 설명
 
--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,22 +1,71 @@
 import java.util.*;
 
-class Solution {
-    static class Node implements Comparable<Node> {
-        private String original;
-        private String head;
-        private int number;
-        private String tail;
+class Solution {    
+    public String[] solution(String[] files) {
+        String[] answer = new String[files.length];
+        FileName[] fileNames = new FileName[files.length];
         
-        public Node(String original, String head, int number, String tail) {
-            this.original = original;
-            this.head = head;
-            this.number = number;
-            this.tail = tail;
+        for (int i = 0; i < files.length; ++i) {
+            fileNames[i] = FileName.of(files[i]);
         }
         
-        @Override
-        public int compareTo(Node other) {
-            // HEAD 비교 (대소문자 구분 없이)
+        Arrays.sort(fileNames);
+        
+        for (int i = 0; i < files.length; ++i) {
+            answer[i] = fileNames[i].original();
+        }
+        
+        return answer;
+    }
+}
+
+class FileName implements Comparable<FileName> {
+    String head;
+    int number;
+    String original;
+    
+    private FileName() {}
+    
+    public static FileName of(String file) {
+        FileName fileName = new FileName();
+        int index = 0, fileLength = file.length();
+        StringBuilder head = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+        
+        while(index < fileLength && !Character.isDigit(file.charAt(index))) {
+            head.append(file.charAt(index));
+            ++index;
+        }
+        
+        while(index < fileLength && Character.isDigit(file.charAt(index))) {
+            number.append(file.charAt(index));
+            ++index;
+        }
+        
+        if (number.toString().equals("")) {
+            number.append("0");
+        } 
+        
+        fileName.original = file;
+        fileName.head = head.toString();
+        fileName.number = Integer.parseInt(number.toString());
+        
+        return fileName;
+    }
+    
+    public String original() {
+        return this.original;
+    }
+    
+    @Override
+    public int compareTo(FileName other) {
+//         if (this.head.compareToIgnoreCase(other.head) == 0) {
+//             return Integer.compare(Integer.parseInt(this.number), Integer.parseInt(other.number));
+//         }
+        
+//         return this.head.compareToIgnoreCase(other.head);
+        
+        // HEAD 비교 (대소문자 구분 없이)
             int headComparison = this.head.compareToIgnoreCase(other.head);
             if (headComparison != 0) {
                 return headComparison;
@@ -24,51 +73,5 @@ class Solution {
 
             // NUMBER 비교
             return Integer.compare(this.number, other.number);
-        }
-
-        public String getOriginal() {
-            return original;
-        }
-    }
-    
-    public String[] solution(String[] files) {
-        List<Node> list = new ArrayList<>();
-        String[] answer = new String[files.length];
-        
-        for (int i = 0; i < files.length; ++i) {
-            String file = files[i];
-            
-            int j = 0;
-            StringBuilder head = new StringBuilder();
-            StringBuilder number = new StringBuilder();
-            StringBuilder tail = new StringBuilder();
-            while (j < file.length() && !Character.isDigit(file.charAt(j))) {
-                head.append(file.charAt(j));
-                ++j;
-            }
-            
-            while (j < file.length() && Character.isDigit(file.charAt(j))) {
-                number.append(file.charAt(j));
-                ++j;
-            }
-            
-            if (j < file.length()) {
-                tail.append(file.substring(j));
-            }
-            
-            if (number.toString().equals("")) {
-                number.append("0");
-            }
-            
-            list.add(new Node(files[i], head.toString(), Integer.parseInt(number.toString()), tail.toString()));
-        }
-        
-        Collections.sort(list);
-        
-        for (int i = 0; i < list.size(); ++i) {
-            answer[i] = list.get(i).getOriginal();
-        }
-        
-        return answer;
     }
 }
