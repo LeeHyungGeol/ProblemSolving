24c63e75cef57e89b526de6fc700a45499499217
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-05-12 15:41:13 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 13.24 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,96 @@
+class Solution {
+    private int[][] Board;
+    private boolean[][] Check;
+    private boolean nothingToRemove = true;
+    private int answer = 0, M = 0, N = 0;
+    
+    public int solution(int m, int n, String[] board) {
+        M = m;
+        N = n;
+        Board = new int[M][N];
+        Check = new boolean[M][N];
+        
+        initBoard(board);
+        
+        while (true) {
+            nothingToRemove = true;
+            
+            for (int i = 0; i < M-1; ++i) {
+                for (int j = 0; j < N-1; ++j) {
+                    if (Board[i][j] == 0) continue;
+                    if (is4BlocksAreSame(i,j)) {
+                        checkBlocks(i,j);
+                    }
+                }
+            }
+            
+            removeBlocks();
+            down();
+            initCheck();
+            
+            
+            if (nothingToRemove) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private void checkBlocks(int x, int y) {
+        Check[x][y] = true;
+        Check[x][y+1] = true;
+        Check[x+1][y] = true;
+        Check[x+1][y+1] = true;        
+    }
+    
+    private void removeBlocks() {
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (Check[i][j]) {
+                    Board[i][j] = 0;
+                    nothingToRemove = false;
+                    ++answer;
+                }
+            }
+        }
+    }
+    
+    private void down() {
+        for (int col = 0; col < N; ++col) {
+            gravity(col);
+        }
+    }
+    
+    private void gravity(int col) {
+        int[] tempArr = new int[M];
+        int endOfTempArr = M-1;
+        
+        for (int i = M-1; i >= 0; --i) {
+            if (Board[i][col] != 0) {
+                tempArr[endOfTempArr--] = Board[i][col];
+            }
+        }
+        
+        for (int i = M-1; i >= 0; --i) {
+            Board[i][col] = tempArr[i];
+        }
+    }
+    
+    private void initCheck() {
+        Check = new boolean[M][N];
+    }
+    
+    private void initBoard(String[] board) {
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                char c = board[i].charAt(j);
+                Board[i][j] = c-'A'+1;
+            }
+        }
+    }
+    
+    private boolean is4BlocksAreSame(int x, int y) {
+        return Board[x][y] == Board[x+1][y] && Board[x][y] == Board[x][y+1] && Board[x][y] == Board[x+1][y+1];
+    }
+}
