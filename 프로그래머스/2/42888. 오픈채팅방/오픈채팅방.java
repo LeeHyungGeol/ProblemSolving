40e2abf5ab98433465551ab14faa0762430b8a09
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-05-13 12:56:13 +0900

    [level 2] Title: 오픈채팅방, Time: 130.11 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,40 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public List<String> solution(String[] record) {
+    private Map<String, String> ID = new HashMap<>();
+    
+    public String[] solution(String[] record) {
         List<String> answer = new ArrayList<>();
-        Map<String, String> map = new HashMap<>();
         
         for (String r : record) {
-            String[] temp = r.split(" ");
-            String command = temp[0];
-            String userId = temp[1];
+            String[] split = r.split(" ");
             
-            if (!command.equals("Leave")) {
-                String nickname = temp[2];
-        
-                if (command.equals("Change")) {
-                    map.replace(userId, nickname);
-                } else {
-                    map.put(userId, nickname);
-                }
+            if ("Enter".equals(split[0]) || "Change".equals(split[0])) {
+                ID.put(split[1], split[2]);
             }
         }
         
         for (String r : record) {
-            String[] temp = r.split(" ");
-            String command = temp[0];
-            String userId = temp[1];
+            String[] split = r.split(" ");
             
-            if (command.equals("Enter")) {
-                answer.add(map.get(userId) + "님이 들어왔습니다.");
-            }
-            if (command.equals("Leave")) {
-                answer.add(map.get(userId) + "님이 나갔습니다.");
+            if ("Enter".equals(split[0])) {
+                answer.add(ID.get(split[1]) + "님이 들어왔습니다.");
+            } 
+            if("Leave".equals(split[0])) {
+                answer.add(ID.get(split[1]) + "님이 나갔습니다.");
             }
         }
         
-        
-        return answer;
+        return answer.stream().toArray(String[]::new);
     }
 }
