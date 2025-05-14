a880a0155d8cb26d979d6ab57ccac50999c363f1
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-05-14 15:17:26 +0900

    [level 1] Title: 실패율, Time: 6.89 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,56 +1,54 @@
 import java.util.*;
 
-// 뿌듯 ^ㅡ^
-// [두 수의 비교 결과에 따른 작동 방식]
-
-// 음수일 경우 : 두 원소의 위치를 교환 안함
-
-// 양수일 경우 : 두 원소의 위치를 교환 함
 class Solution {
-    class Failure {
-        private int stageNumber;
-        private double fail;
+    class StageInfo implements Comparable<StageInfo> {
+        int index;
+        double failure;
         
-        public Failure(int stageNumber, double fail) {
-            this.stageNumber = stageNumber;
-            this.fail = fail;
+        public StageInfo(int index, double failure) {
+            this.index = index;
+            this.failure = failure;
         }
         
-        public int getStageNumber() {
-            return stageNumber;
-        }
-        
-        public double getFail() {
-            return fail;
+        @Override
+        public int compareTo(StageInfo other) {
+            if (this.failure == other.failure) {
+                return this.index - other.index;
+            }
+            return Double.compare(other.failure, this.failure);
         }
     }
     
     public int[] solution(int N, int[] stages) {
-        List<Failure> list = new LinkedList<>();
-        int[] answer = new int[N];
+        int[] completed = new int[N+2];
         
-        for (int i = 1; i <= N; ++i) {
-            int notClear = 0, reach = 0;
-            
-            for (int j = 0; j < stages.length; ++j) {
-                if (stages[j] == i) ++notClear;
-                if (stages[j] >= i) ++reach;
-            }
-            if (reach == 0) list.add(new Failure(i, 0));
-            else list.add(new Failure(i, (double)notClear/reach));
+        for (int player : stages) {
+            ++completed[player];
         }
         
-        Collections.sort(list, new Comparator<Failure>(){
-            @Override
-            public int compare(Failure o1, Failure o2) {
-                if (o1.getFail() < o2.getFail()) return 1;
-                else if (o1.getFail() > o2.getFail()) return -1;
-                else return 0;
+        int reached = stages.length;
+        double[] failure = new double[N+1];
+        for (int i = 1; i < N+1; ++i) {
+            reached -= completed[i-1];
+            if (reached == 0) {
+                failure[i] = 0;
+            } else {
+                failure[i] = (double)completed[i]/reached;
             }
-        });
+        }
         
-        for (int i = 0; i < list.size(); ++i) answer[i] = list.get(i).getStageNumber();
-
+        StageInfo[] infos = new StageInfo[N];
+        for (int i = 1; i < N+1; ++i) {
+            infos[i-1] = new StageInfo(i, failure[i]);
+        }
+        
+        Arrays.sort(infos);
+        
+        int[] answer = new int[N];
+        for (int i = 0; i < N; ++i) {
+            answer[i] = infos[i].index;
+        }
+        
         return answer;
     }
 }
