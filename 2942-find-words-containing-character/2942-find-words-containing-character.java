532c143995e8fb70aced5eedbfb6d4fae2fe08b4
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-05-24 23:40:10 +0900

    Time: 2 ms (52.59%), Space: 45.1 MB (39.73%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> answer = new ArrayList<>();
+        String s = x+"";
+        for (int i = 0; i < words.length; ++i) {
+            if (words[i].contains(s)) {
+                answer.add(i);
+            }
+        }
+
+        return answer;
+    }
+}
