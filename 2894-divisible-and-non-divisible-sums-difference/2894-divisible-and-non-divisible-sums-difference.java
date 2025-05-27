323fa6104b6ac6137b5eabe61d8ff7257e1dc16f
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-05-27 12:24:55 +0900

    Time: 1 ms (74.69%), Space: 40.7 MB (68.69%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
@@ -1,15 +1,15 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
+        int answer = 0;
 
         for (int i = 1; i <= n; ++i) {
             if (i%m == 0) {
-                num2 += i;
+                answer -= i;
             } else {
-                num1 += i;
+                answer += i;
             }
         }
 
-        return num1-num2;
+        return answer;
     }
 }
