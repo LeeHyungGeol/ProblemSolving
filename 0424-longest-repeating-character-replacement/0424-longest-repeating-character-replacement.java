5522dadcec69d7f82e529d0eae96e312a2a08289
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-07-03 23:34:20 +0900

    Time: 6 ms (98.05%), Space: 44.9 MB (59.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,17 +2,17 @@ import java.util.HashMap;
 
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> counter = new HashMap<>();
+        int[] counter = new int[26];
         int answer = 0, left = 0, maxFreq = 0;
 
         for (int right = 0; right < s.length(); ++right) {
             char rightChar = s.charAt(right);
-            counter.put(rightChar, counter.getOrDefault(rightChar, 0) + 1);
-            maxFreq = Math.max(maxFreq, counter.get(rightChar));
+            ++counter[rightChar-'A'];
+            maxFreq = Math.max(maxFreq, counter[rightChar-'A']);
 
             while ((right - left + 1) - maxFreq > k) {
                 char leftChar = s.charAt(left);
-                counter.put(leftChar, counter.get(leftChar) - 1);
+                --counter[leftChar-'A'];
                 ++left;
             }
 
