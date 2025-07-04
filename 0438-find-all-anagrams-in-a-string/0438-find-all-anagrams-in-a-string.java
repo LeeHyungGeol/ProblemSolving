514e32b38a136f49f21efac078191585b3f2df10
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-07-04 18:00:52 +0900

    Time: 24 ms (42.96%), Space: 44.8 MB (81.78%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public List<Integer> findAnagrams(String s, String t) {
+        List<Integer> answer = new ArrayList<>();
+
+        if(t.length()> s.length()) {
+            return answer;
+        }
+
+        Map<Character, Integer> pattern = new HashMap<>();
+        for(char c : t.toCharArray()){
+            pattern.put(c, pattern.getOrDefault(c, 0) + 1);
+        }
+        int counter = pattern.size(), left = 0, right = 0;
+        
+        while(right < s.length()){
+            char c = s.charAt(right);
+            if(pattern.containsKey(c)){
+                pattern.put(c, pattern.get(c)-1);
+                if (pattern.get(c) == 0) {
+                    --counter;
+                }
+            }
+            ++right;
+            
+            while(counter == 0){
+                char tempc = s.charAt(left);
+                if(pattern.containsKey(tempc)){
+                    pattern.put(tempc, pattern.get(tempc) + 1);
+                    if(pattern.get(tempc) > 0){
+                        ++counter;
+                    }
+                }
+                if(right-left == t.length()){
+                    answer.add(left);
+                }
+                ++left;
+            }
+        }
+
+        return answer;
+    }
+}
