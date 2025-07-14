7b0446ab240d5f7a72ba6ecaa1527de8eed9008b
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-07-14 17:15:49 +0900

    Time: 24 ms (50.29%), Space: 55.3 MB (49.23%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -3,17 +3,17 @@ import java.util.Map;
 
 class Solution {
     public int findMaxLength(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-		int answer = 0, count = 0;
+        Map<Integer, Integer> counter = new HashMap<>();
+		int answer = 0, sum = 0;
 
-		map.put(0, -1);
+		counter.put(0, -1);
 
 		for (int i = 0; i < nums.length; i++) {
-			count += (nums[i] == 1 ? 1 : -1);
-			if (map.containsKey(count)) {
-				answer = Math.max(answer, i - map.get(count));
+			sum += (nums[i] == 1 ? 1 : -1);
+			if (counter.containsKey(sum)) {
+				answer = Math.max(answer, i - counter.get(sum));
 			} else {
-				map.put(count, i);
+				counter.put(sum, i);
 			}
 		}
 
