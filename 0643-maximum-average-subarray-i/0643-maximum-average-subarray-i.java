ee2bb928a7cf156aac75ff9a57a389ea5c06ba66
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-09-05 01:28:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0, count = 0;
+        double answer = 0;
+
+        for (int left = 0, right = 0; right< n; ++right) {
+            sum += nums[right];
+            ++count;
+
+            while (count == k) {
+                answer = Math.max(answer, (double)sum/k);
+
+                sum -= nums[left++];
+                --count;
+            }
+        }
+
+        return answer;
+    }
+}
