e82310c1eafc194fc79fc59081da1fb67ccf667a
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-09-04 21:02:54 +0900

    Time: 101 ms (16.82%), Space: 45.9 MB (11.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,35 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
-                s.length() < t.length()) {
-            return new String();
-        }
-        int[] map = new int[128];
-        int count = t.length();
-        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
-        /// UPVOTE !
+        int n = s.length();
+        int m = 0; // 고유 문자의 개수
+
+        // 각 문자별 필요한 개수 저장
+        int[] req = new int[256];
         for (char c : t.toCharArray()) {
-            map[c]++;
-        }
-
-        char[] chS = s.toCharArray();
-
-        while (end < chS.length) {
-            if (map[chS[end++]]-- > 0) {
-                count--;
-            }
-            while (count == 0) {
-                if (end - start < minLen) {
-                    startIndex = start;
-                    minLen = end - start;
-                }
-                if (map[chS[start++]]++ == 0) {
-                    count++;
-                }
+            if (++req[c] == 1) { // 처음 등장하는 문자라면
+                m++; // 고유 문자 개수 증가
             }
         }
 
-        return minLen == Integer.MAX_VALUE ? new String() :
-                new String(chS, startIndex, minLen);
+        String res = "";
+        int charCount = 0; // 요구사항을 만족시키는 글자의 개수
+        int[] cnt = new int[256]; // 현재 윈도우의 각 문자별 개수
+
+        for (int L = 0, R = 0; R < n; R++) {
+            // update right
+            if (++cnt[s.charAt(R)] == req[s.charAt(R)]) {
+                charCount++; // 만족시키는 글자의 개수
+            }
+
+            // update left
+            while (charCount == m) {
+                // update answer - [L, R] (현재 유효한 윈도우)
+                int len = R - L + 1;
+                if (res.isEmpty() || res.length() > len) {
+                    res = s.substring(L, R+1);
+                }
+
+                // 왼쪽 문자 제거
+                int c = s.charAt(L++);
+                if (--cnt[c] < req[c]) {
+                    charCount--;
+                }
+            }
+        }
+
+        return res;
     }
 }
