1908d1e5a569c58e3f728fe1682631be7af491fd
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-09-04 22:03:33 +0900

    Time: 2 ms (99.85%), Space: 45 MB (67.21%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n = s.length();
-        int m = 0; // 고유 문자의 개수
-
-        // 각 문자별 필요한 개수 저장
-        int[] req = new int[256];
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
+        }
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+        /// UPVOTE !
         for (char c : t.toCharArray()) {
-            if (++req[c] == 1) { // 처음 등장하는 문자라면
-                m++; // 고유 문자 개수 증가
-            }
+            map[c]++;
         }
 
-        String res = "";
-        int charCount = 0; // 요구사항을 만족시키는 글자의 개수
-        int[] cnt = new int[256]; // 현재 윈도우의 각 문자별 개수
+        char[] chS = s.toCharArray();
 
-        for (int L = 0, R = 0; R < n; R++) {
-            // update right
-            if (++cnt[s.charAt(R)] == req[s.charAt(R)]) {
-                charCount++; // 만족시키는 글자의 개수
+        while (end < chS.length) {
+            if (map[chS[end++]]-- > 0) {
+                count--;
             }
-
-            // update left
-            while (charCount == m) {
-                // update answer - [L, R] (현재 유효한 윈도우)
-                int len = R - L + 1;
-                if (res.isEmpty() || res.length() > len) {
-                    res = s.substring(L, R+1);
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
                 }
-
-                // 왼쪽 문자 제거
-                int c = s.charAt(L++);
-                if (--cnt[c] < req[c]) {
-                    charCount--;
+                if (map[chS[start++]]++ == 0) {
+                    count++;
                 }
             }
         }
 
-        return res;
+        return minLen == Integer.MAX_VALUE ? new String() :
+                new String(chS, startIndex, minLen);
     }
 }
