25c61e92daf8c6d8749b738bfeb1bac7dc118b75
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-09-07 11:57:15 +0900

    Time: 2 ms (79.59%), Space: 46.1 MB (79.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0;
-
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index++] = temp;
+        int n = nums.length;
+    
+        for (int left = 0, right = 0; right < n; ++right) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                ++left;
             }
         }
     }
