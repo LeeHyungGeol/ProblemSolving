d8a57b854d2fecff29794242af17f6dc4e120a91
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-09-07 12:11:46 +0900

    Time: 2 ms (68.53%), Space: 41.6 MB (55.12%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) {
+            return false;
+        }
+
         int sp = 0;
         int tp = 0;
-
+        
         while (sp < s.length() && tp < t.length()) {
             if (s.charAt(sp) == t.charAt(tp)) {
                 sp++;
-            }
+            } 
             tp++;
         }
 
-        return sp == s.length();        
+        return sp == s.length();
     }
 }
