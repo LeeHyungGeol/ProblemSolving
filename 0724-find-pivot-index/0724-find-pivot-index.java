100f55691ca1abbe7915ab32dcadfc14c7bf0605
이형걸 <56071088+LeeHyungGeol@users.noreply.github.com>
2025-09-16 20:36:03 +0900

    Time: 1 ms (92.28%), Space: 45.8 MB (36.92%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
-        int[] leftSum = new int[n+1];
-        int[] rightSum = new int[n+1];
+        int[] sum = new int[n+1];
+        int total = 0;
         int answer = -1;
 
         for (int i = 0; i < n; ++i) {
-            leftSum[i+1] = leftSum[i] + nums[i];
-        }
-
-        for (int i = n-1; i >= 0; --i) {
-            rightSum[i] = rightSum[i+1] + nums[i];
+            sum[i+1] = sum[i] + nums[i];
+            total += nums[i];
         }
 
         for (int i = 0; i < n; ++i) {
-            if (leftSum[i] == rightSum[i+1]) {
+            if (sum[i] == total-sum[i]-nums[i]) {
                 answer = i;
                 break;
             }
